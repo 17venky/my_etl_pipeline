853d787f5f39639e7551aeb0a919b52db3f80c13
17venky <venkateshreddy0086@gmail.com>
2025-04-05 18:51:19 +0530

    Update __init__setup.py

--- notebooks/__init__setup.py
+++ notebooks/__init__setup.py
@@ -4,8 +4,8 @@ import os
 spark = SparkSession.builder.appName("ETL_Pipeline").getOrCreate()
 
 BASE_PATH = "/Workspace/Repos/17venky/my_etl_pipeline"
-CONFIG_PATH = os.path.join(BASE_PATH, "config/pipeline_config.json")
-FILE_LAYOUT = os.path.join(BASE_PATH, "file_layout/file_layout.json")
+FILE_LAYOUT = os.path.join(BASE_PATH, "input_data/file_layout.json")
+CONFIG_PATH = os.path.join(BASE_PATH, "input_data/pipeline_config.json")
 INPUT_PATH = os.path.join(BASE_PATH, "input_data/member_dataset.txt")
 BRONZE_PATH = os.path.join(BASE_PATH, "bronze")
 SILVER_PATH = os.path.join(BASE_PATH, "silver")
