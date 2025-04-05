df8a8aa69596e73fe6bf06e0d3645a4a82fc6e4a
17venky <venkateshreddy0086@gmail.com>
2025-04-05 22:35:15 +0530

    Delete notebooks directory

--- notebooks/bronze_extraction.py
+++
@@ -1,9 +0,0 @@
-from etl_pipeline.utils import read_json_file
-from etl_pipeline.extractor import extract_fixed_width
-from etl_pipeline.validator import apply_validations
-from etl_pipeline.writer import write_to_path
-
-layout = read_json_file(spark, FILE_LAYOUT)
-df_bronze = extract_fixed_width(spark, INPUT_PATH, layout)
-df_validated = apply_validations(df_bronze, layout)
-write_to_path(df_validated, BRONZE_PATH)
--- notebooks/gold_reporting.py
+++
@@ -1,2 +0,0 @@
-df_silver = spark.read.parquet(SILVER_PATH)
-df_silver.write.mode("overwrite").format("delta").save(GOLD_PATH)
