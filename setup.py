717c99fbfa301ddccfd12354b73b5dfdedaa7f5e
17venky <venkateshreddy0086@gmail.com>
2025-04-07 19:25:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pyspark',
+        "pytest",
+        "pandas",
         'jsonschema',
         # Add other dependencies
     ],
