57d7ceeb139180000460964fc4ab587068cbd506
17venky <venkateshreddy0086@gmail.com>
2025-04-08 19:54:38 +0530

    Update main_gold.py

--- Scripts/main_gold.py
+++ Scripts/main_gold.py
@@ -2,14 +2,15 @@ from pyspark.sql import SparkSession
 import os
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Gold Job").getOrCreate()
+    spark = SparkSession.builder.appName("Gold Job").config("spark.mongodb.output.uri", "mongodb+srv://venkateshreddy0086:eaShASiYsS1Kakxc@cluster0.iu7jsc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").getOrCreate()
 
     #BASE_PATH = "/Volumes/workspace/default/venky"
     BASE_PATH = "/home/hemanthkr004/my_project/my_etl_pipeline/input_data"
-    SILVER_PATH = os.path.join(BASE_PATH, "silver")
+    SILVER_PATH = os.path.join(BASE_PATH, "silver") 
     GOLD_PATH = os.path.join(BASE_PATH, "gold")
 
     df_silver = spark.read.parquet(SILVER_PATH)
     df_silver.write.mode("overwrite").format("parquet").save(GOLD_PATH)
-
+    #df_silver.write.format("mongodb").mode("overwrite").save()
+    
     spark.stop()
